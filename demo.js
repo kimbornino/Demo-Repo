516df45f4b9f1154e0ebb6629c030c82408365d1
Kimbornino <kimbornino@gmail.com>
2019-03-12 09:52:54 -0500

    This is an example using git commmit
    The purpose of this message is to practice creating a longer and more
    professional message on gitHub.  The body of this message is supposed to
    contain a more detailed description and be one or more paragraphs.
    - bullet points are also accceptable
    
    It also  may contain a footer.

--- demo.js
+++ demo.js
@@ -1,4 +1,21 @@
-console.log("hello world");
-console.log("Kimberly");
-console.log (31);
-window.alert("Wisconsin");
+let ratePerHour = getanswer("Enter your hourly rate");
+
+let hoursWorked = getanswer("Enter your hours worked:");
+
+function getanswer(message) {
+  let answer = prompt(message);
+  return answer;
+}
+
+if (hoursWorked<=40) {
+  console.log (ratePerHour*hoursWorked)
+}
+else {
+  let otPay = ratePerHour * 1.5;
+  let otHours = hoursWorked-40;
+  let overtimeTotal = otPay * otHours;
+  let regPay = 40* ratePerHour;
+  let total = regPay + overtimeTotal;
+
+  console.log (total);
+}
